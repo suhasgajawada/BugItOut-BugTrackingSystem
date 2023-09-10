d99e358455b0f739317ef8835dc61524222260ae
akki0510 <singh.aakriti0510@gmail.com>
2023-09-10 22:53:14 +0530

    changes in sql script

--- bugbase.sql
+++ bugbase.sql
@@ -10,7 +10,7 @@ CREATE TABLE users(
   `loggedIn` BOOLEAN NOT NULL DEFAULT FALSE,
   `userType` ENUM('Project Manager', 'Developer', 'Tester') NOT NULL,
   PRIMARY KEY (`userId`),
-  UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE);
+  UNIQUE INDEX `email_UNIQUE` (`email` ASC));
 
 /* Project Table */
 CREATE TABLE projects(
@@ -28,8 +28,8 @@ CREATE TABLE team_members(
   `userId` INT NOT  NULL ,
   `projectId` INT NOT NULL ,
   PRIMARY KEY (`teamId`),
-  INDEX `userId` (`userId` ASC) VISIBLE,
-  INDEX `projectId` (`projectId` ASC) VISIBLE,
+  INDEX `userId` (`userId` ASC) ,
+  INDEX `projectId` (`projectId` ASC) ,
     FOREIGN KEY (`userId`)
     REFERENCES users(`userId`),
     FOREIGN KEY (`projectId`)
@@ -51,10 +51,10 @@ CREATE TABLE bug(
   `status` ENUM('Open', 'Closed') NOT NULL,
   `severity` ENUM('Critical', 'Major', 'Minor', 'Trivial') NOT NULL,
   PRIMARY KEY (`bug_id`),
-  INDEX `projectId` (`projectId` ASC) VISIBLE,
-  INDEX `created_by` (`created_by` ASC) VISIBLE,
-  INDEX `assigned_to` (`assigned_to` ASC) VISIBLE,
-  INDEX `closed_by` (`closed_by` ASC) VISIBLE,
+  INDEX `projectId` (`projectId` ASC) ,
+  INDEX `created_by` (`created_by` ASC) ,
+  INDEX `assigned_to` (`assigned_to` ASC) ,
+  INDEX `closed_by` (`closed_by` ASC) ,
     FOREIGN KEY (`projectId`)
     REFERENCES projects(`projectId`),
     FOREIGN KEY (`created_by`)
@@ -65,36 +65,4 @@ CREATE TABLE bug(
     REFERENCES users(`userId`)
     );
     
-    CREATE TABLE `bug`(
-  `bug_id` INT NOT NULL AUTO_INCREMENT,
-  `bug_title` VARCHAR(255) NOT NULL,
-  `bug_description` TEXT NOT NULL,
-  `projectId` INT NOT NULL,
-  `created_by` INT NOT NULL,
-  `open_date` TIMESTAMP NOT NULL,
-  `assigned_to` INT NULL DEFAULT NULL,
-  `marked_for_closing` TINYINT(1) NOT NULL DEFAULT '0',
-  `closed_by` INT NULL DEFAULT NULL,
-  `closed_on` TIMESTAMP NULL DEFAULT NULL,
-  `status` ENUM('Open', 'Closed') NOT NULL,
-  `severity` ENUM('Critical', 'Major', 'Minor', 'Trivial') NOT NULL,
-  PRIMARY KEY (`bug_id`),
-  INDEX `projectId` (`projectId` ASC) VISIBLE,
-  INDEX `created_by` (`created_by` ASC) VISIBLE,
-  INDEX `assigned_to` (`assigned_to` ASC) VISIBLE,
-  INDEX `closed_by` (`closed_by` ASC) VISIBLE,
-  FOREIGN KEY (`projectId`)
-    REFERENCES `projects`(`projectId`),
-  FOREIGN KEY (`created_by`)
-    REFERENCES `users`(`userId`),
-  FOREIGN KEY (`assigned_to`)
-    REFERENCES `users`(`userId`),
-  FOREIGN KEY (`closed_by`)
-    REFERENCES `users`(`userId`)
-)
-
-
-
-
-
-
+    
